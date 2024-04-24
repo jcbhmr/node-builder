d23ac63145c6585392001c6e8e2bad8b8a6283ba
Jacob Hummer <jcbhmr@outlook.com>
2024-04-24 21:33:15 +0000

    closer

---
+++ test/hello-world/index.test.js
@@ -0,0 +1,7 @@
+import { $ } from "execa"
+import { resolve } from "node:path"
+
+await $({ stdio: "inherit", cwd: import.meta.dirname, env: {
+    CC: "clang-18",
+    CXX: "clang++-18"
+}, shell: true })`$CXX -I${resolve("node/src")} -I${resolve("node/deps/v8/include")} main.cpp ${resolve("node/out/Release/obj.target/libnode.a")}`
---
+++ test/hello-world/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "node.h"
+
+int main(int argc, char* argv[]) {
+    node::Start(argc, argv);
+    return 0;
+}
